fd0020e6450e0d3d2c71ef8df39ccb52c40d0024
jyf <ji_yf@Ctrip.com>
2018-07-21 11:10:51 +0800

    commit

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
-const path = require('path')
-const fs = require('fs')
-const requireHacker = require('require-hacker')
+const path = require('path');
+const fs = require('fs');
+const requireHacker = require('require-hacker');
 const withLess = require('@zeit/next-less');
 
 
-function setupRequireHacker () {
-  const webjs = '.web.js'
-  const webModules = ['antd-mobile', 'rmc-picker'].map(m => path.join('node_modules', m))
+function setupRequireHacker() {
+  const webjs = '.web.js';
+  const webModules = ['antd-mobile', 'rmc-picker'].map(m => path.join('node_modules', m));
 
-  requireHacker.hook('js', filename => {
-    if (filename.endsWith(webjs) || webModules.every(p => !filename.includes(p))) return
+  requireHacker.hook('js', (filename) => {
+    if (filename.endsWith(webjs) || webModules.every(p => !filename.includes(p))) return;
 
-    const webFilename = filename.replace(/\.js$/, webjs)
-    if (!fs.existsSync(webFilename)) return
+    const webFilename = filename.replace(/\.js$/, webjs);
+    if (!fs.existsSync(webFilename)) return;
 
-    return fs.readFileSync(webFilename, { encoding: 'utf8' })
-  })
+    return fs.readFileSync(webFilename, { encoding: 'utf8' });
+  });
 
-  requireHacker.hook('svg', filename => {
-    return requireHacker.to_javascript_module_source(`#${path.parse(filename).name}`)
-  })
+  requireHacker.hook('svg', (filename) => {
+    return requireHacker.to_javascript_module_source(`#${path.parse(filename).name}`);
+  });
 }
 
-setupRequireHacker()
+setupRequireHacker();
 
-function moduleDir (m) {
-  return path.dirname(require.resolve(`${m}/package.json`))
+function moduleDir(m) {
+  return path.dirname(require.resolve(`${m}/package.json`));
 }
 
 module.exports = withLess({
@@ -34,30 +34,30 @@ module.exports = withLess({
     javascriptEnabled: true,
   },
   webpack: (config, { dev }) => {
-    config.resolve.extensions = ['.web.js', '.js', '.json']
+    config.resolve.extensions = ['.web.js', '.js', '.json'];
 
     config.module.rules.push(
       {
         test: /\.(svg)$/i,
         loader: 'emit-file-loader',
         options: {
-          name: 'dist/[path][name].[ext]'
+          name: 'dist/[path][name].[ext]',
         },
         include: [
           moduleDir('antd-mobile'),
-          __dirname
-        ]
+          __dirname,
+        ],
       },
       {
         test: /\.(svg)$/i,
         loader: 'svg-sprite-loader',
         include: [
           moduleDir('antd-mobile'),
-          __dirname
-        ]
-      }
-    )
+          __dirname,
+        ],
+      },
+    );
 
-    return config
-  }
+    return config;
+  },
 });
